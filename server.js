5575a61045c57b282932dc0bf157c1aa3e88d579
chrismiWeb <chrismibiteso@gmail.com>
2025-03-06 02:44:00 -0800

    added cors

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const router = require('./routes/handler');
 const connectDb = require('./database/connectDB');
 const app = express();
 const port = 1050;
+const cors = require('cors')
 
 
 // exxpress to json
 app.use(express.json())
 app.use("/", router)
 
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions))
 
 app.get('/', (req, res)=>(
     console.log("app is running fine ")

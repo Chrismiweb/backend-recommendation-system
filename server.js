fe5473171c42ae4c5679a640951ce182dd713d5e
chrismiWeb <chrismibiteso@gmail.com>
2025-03-06 11:27:19 -0800

    corrected errors

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = require('./routes/handler');
 const connectDb = require('./database/connectDB');
 const app = express();
-const port = 1050;
+// const port = 1050;
 const cors = require('cors')
 
+const port = process.env.PORT || 1050;
 
 // exxpress to json
 app.use(express.json())

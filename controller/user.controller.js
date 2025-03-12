ee85bd504b78072125dbdb56ba0cba1129dcf6d4
chrismiWeb <chrismibiteso@gmail.com>
2025-03-11 22:28:56 -0700

    added user info in login output

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -68,7 +68,7 @@ const loginUser = async(req, res)=>{
     }
     const token = jwt.sign({ userId : existingUser._id}, process.env.jwt_secret,{ expiresIn: "1h"});
 
-    res.status(201).json({message: " login successfully", token})
+    res.status(201).json({message: " login successfully", token, existingUser})
 
 
     
